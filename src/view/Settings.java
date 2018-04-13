9fd18953c3ad0cba06ac1da12164ec0ae6493883
Jadie-Adams <36213628+Jadie-Adams@users.noreply.github.com>
2018-04-12 20:04:25 -0600

    Intructions added to settings
    Added an instruction pupup for all three games

--- src/view/Settings.java
+++ src/view/Settings.java
@@ -15,6 +15,7 @@ import java.awt.Insets;
 import javax.swing.border.LineBorder;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JOptionPane;
 
 public class Settings extends JPanel
 {
@@ -24,6 +25,12 @@ public class Settings extends JPanel
 	private JButton changePassButton;
 	private JButton logoutButton;
 	private JButton backButton;
+	private JButton game1instructions;
+	private JLabel g1instruct;
+	private JButton game2instructions;
+	private JLabel g2instruct;
+	private JButton game3instructions;
+	private JLabel g3instruct;
 
 	public Settings(Controller base)
 	{
@@ -32,7 +39,16 @@ public class Settings extends JPanel
 		changePassButton = new JButton(" Change Password ");
 		logoutButton = new JButton(" Log Out ");
 		backButton = new JButton(" BACK ");
-
+		game1instructions = new JButton("<html>   Fish Game<br/> Instructions   <html>");
+		g1instruct = new JLabel("<html>To play click on the fish that has the <br/> answer to the math question. <br/> "
+				+ "<br/>The game ends when time runs out or the <br/> correct fish swims off screen.</html>");
+		game2instructions = new JButton("<html> Jellyfish Game<br/> Instructions <html>");
+		g2instruct = new JLabel("<html>To play use the side arrow keys to help the <br/> jellyfish float down to the correct answer. <br/> "
+				+ "<br/>The down arrow will help the jellyfish go faster <br/> and the up arrow will slow it back down. <br/>"
+				+ " <br/>The game ends when time runs out. </html>");
+		game3instructions = new JButton("<html> Shark Game<br/> Instructions <html>");
+		g3instruct = new JLabel("<html>To play use the arrow keys to help the <br/> shark swim to the correct answer. <br/> "
+				+ "<br/>The game ends when time runs out.</html>");
 		setUpLayout();
 		setUpListeners();
 		setUpPanel();
@@ -61,6 +77,16 @@ public class Settings extends JPanel
 		setBorder(new LineBorder(new Color(70, 130, 180), 10));
 		setForeground(new Color(135, 206, 235));
 		setBackground(new Color(245, 245, 245));
+		
+		g1instruct.setFont(new Font("Arial", Font.PLAIN, 30));
+		g1instruct.setForeground(new Color(70, 130, 180));
+		g1instruct.setBackground(new Color(208, 243, 255));
+		g2instruct.setFont(new Font("Arial", Font.PLAIN, 30));
+		g2instruct.setForeground(new Color(70, 130, 180));
+		g2instruct.setBackground(new Color(208, 243, 255));
+		g3instruct.setFont(new Font("Arial", Font.PLAIN, 30));
+		g3instruct.setForeground(new Color(70, 130, 180));
+		g3instruct.setBackground(new Color(208, 243, 255));
 
 		changePassButton.setFont(new Font("Arial", Font.PLAIN, 30));
 		changePassButton.setForeground(new Color(70, 130, 180));
@@ -96,9 +122,49 @@ public class Settings extends JPanel
 		gbc_backButton.anchor = GridBagConstraints.NORTHWEST;
 		gbc_backButton.gridx = 0;
 		gbc_backButton.gridy = 0;
+		
+		game1instructions.setFont(new Font("Arial", Font.PLAIN, 25));
+		game1instructions.setForeground(new Color(70, 130, 180));
+		game1instructions.setBackground(new Color(0, 0, 0));
+		game1instructions.setFocusPainted(false);
+		game1instructions.setContentAreaFilled(false);
+		game1instructions.setBorder(new LineBorder(new Color(70, 130, 180), 2));
+		GridBagConstraints gbc_game1instructions = new GridBagConstraints();
+		gbc_game1instructions.insets = new Insets(20, 20, 5, 5);
+		gbc_game1instructions.anchor = GridBagConstraints.NORTH;
+		gbc_game1instructions.gridx = 0;
+		gbc_game1instructions.gridy = 4;
+		
+		game2instructions.setFont(new Font("Arial", Font.PLAIN, 25));
+		game2instructions.setForeground(new Color(70, 130, 180));
+		game2instructions.setBackground(new Color(0, 0, 0));
+		game2instructions.setFocusPainted(false);
+		game2instructions.setContentAreaFilled(false);
+		game2instructions.setBorder(new LineBorder(new Color(70, 130, 180), 2));
+		GridBagConstraints gbc_game2instructions = new GridBagConstraints();
+		gbc_game2instructions.insets = new Insets(20, 20, 5, 5);
+		gbc_game2instructions.anchor = GridBagConstraints.NORTH;
+		gbc_game2instructions.gridx = 1;
+		gbc_game2instructions.gridy = 4;
+		
+		game3instructions.setFont(new Font("Arial", Font.PLAIN, 25));
+		game3instructions.setForeground(new Color(70, 130, 180));
+		game3instructions.setBackground(new Color(0, 0, 0));
+		game3instructions.setFocusPainted(false);
+		game3instructions.setContentAreaFilled(false);
+		game3instructions.setBorder(new LineBorder(new Color(70, 130, 180), 2));
+		GridBagConstraints gbc_game3instructions = new GridBagConstraints();
+		gbc_game3instructions.insets = new Insets(20, 20, 5, 5);
+		gbc_game3instructions.anchor = GridBagConstraints.NORTH;
+		gbc_game3instructions.gridx = 2;
+		gbc_game3instructions.gridy = 4;
+		
 		add(changePassButton, gbc_changePassButton);
 		add(logoutButton, gbc_logoutButton);
 		add(backButton, gbc_backButton);
+		add(game1instructions, gbc_game1instructions);
+		add(game2instructions, gbc_game2instructions);
+		add(game3instructions, gbc_game3instructions);
 	}
 
 	private void setUpListeners()
@@ -127,6 +193,31 @@ public class Settings extends JPanel
 				base.returnToMenu();
 			}
 		});
+		
+		game1instructions.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent onClick)
+			{
+				JOptionPane.showMessageDialog(base.messagePanel, g1instruct, "Game 1 Instructions",JOptionPane.PLAIN_MESSAGE);
+			}
+		});
+		
+		game2instructions.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent onClick)
+			{
+				JOptionPane.showMessageDialog(base.messagePanel, g2instruct, "Game 2 Instructions",JOptionPane.PLAIN_MESSAGE);
+			}
+		});
+		
+		game3instructions.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent onClick)
+			{
+				JOptionPane.showMessageDialog(base.messagePanel, g3instruct, "Game 3 Instructions",JOptionPane.PLAIN_MESSAGE);
+			}
+		});
+		
 	}
 
 }
