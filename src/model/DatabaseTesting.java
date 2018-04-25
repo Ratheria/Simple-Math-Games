a6f9e41a942fcdb0a71d6d78e1c7ee21642bb97d
walkersorlie <walkersorlie@gmail.com>
2018-04-24 18:16:12 -0600

    Started database unit testing

---
+++ src/model/DatabaseTesting.java
@@ -0,0 +1,31 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+import com.mysql.jdbc.ResultSetMetaData;
+
+import adapter.Controller;
+
+public class DatabaseTesting {
+
+	@Test
+	public void testCorrectDatabase() {
+		Controller control = new Controller();
+		MySQLData database = new MySQLData(control);
+
+		try {
+			ResultSet result = database.query("SELECT DATABASE();");
+			while (result.next()) {
+				String name = result.getString(1);
+				assertEquals(name, "smg");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
